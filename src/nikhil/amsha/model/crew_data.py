2362a17bac967d05b108ee44971c280904fba148
NikhilVijayakumar <nikhil.vijaykumar@gmail.com>
2025-05-20 22:52:26 +0530

    add model_config to CrewData

--- src/nikhil/amsha/model/crew_data.py
+++ src/nikhil/amsha/model/crew_data.py
@@ -1,10 +1,11 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 from nikhil.amsha.repo.agent_repo import AgentRepository
 from nikhil.amsha.repo.task_repo import TaskRepository
 
 
 class CrewData(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
     llm: str = Field(..., description="Model name")
     module_name: str = Field(..., description="The name of module")
     agent_repo: AgentRepository = Field(..., description="Agent repo")
