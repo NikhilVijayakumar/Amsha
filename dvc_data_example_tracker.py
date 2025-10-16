f92e6b66b1310cf2e10e3edc577917355e8e900a
NikhilVijayakumar <nikhil.vijaykumar@gmail.com>
2025-10-16 12:47:36 +0530

    chore: Initial data tracking with DVC

--- dvc_data_example_tracker.py
+++ dvc_data_example_tracker.py
@@ -2,7 +2,7 @@
 from nikhil.amsha.toolkit.mlops.dvc_data_tracker import DVCDataTracker
 
 if __name__ == "__main__":
-    CONFIG_FILE_PATH = "config/app_config.yaml"
+    CONFIG_FILE_PATH = "../../crew_forge/example/config/app_config.yaml"
 
     print("=============================================")
     print("      Automated DVC Data Tracking Tool")
